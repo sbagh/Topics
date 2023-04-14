e2fe30116e1fac4de6c8d08723d6b919c130d80c
Sami <52921619+sbagh@users.noreply.github.com>
2023-04-14 10:38:31 -0400

    setup GraphQL

--- crashcourses/python/OOP/classes.py
+++ crashcourses/python/OOP/classes.py
@@ -1,9 +1,18 @@
 #classes
 
 class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        
+    def add_one(self, x):
+        return x+1
+    def get_name
     def bark(self):
         print("bark")
         
-d = Dog()
-d.bark()
-print(type(d))
+d = Dog("tim")
+d2 = Dog("scoobs")
+
+print(d.name)
+print(d2.name)
+

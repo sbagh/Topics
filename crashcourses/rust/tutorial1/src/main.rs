dc5e14882a521297774e06dfb1cc1b1b4408682a
Sami <52921619+sbagh@users.noreply.github.com>
2023-05-02 15:30:38 -0400

    rust variables and const

--- crashcourses/rust/tutorial1/src/main.rs
+++ crashcourses/rust/tutorial1/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    // implicit type
+    let x = 4;
+    println!("x is : {}", x);
+    //mutable variable
+    let mut y = 4;
+    y = 5;
+    println!("y is : {}", y);
+    {
+        let x = 2;
+        println!("x is: {}", x);
+    }
+    let x = 3;
+    println!("x is : {}", x);
+    let x = "hello";
+    println!("x is {}", x);
+    // let y = 'hi';
+    // print("y i s {}", y);
+    //constants
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("{}", SECONDS_IN_MINUTE);
 }

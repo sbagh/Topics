be8b7f290b6f7990eb61115109dcea86979a07e9
Sami <52921619+sbagh@users.noreply.github.com>
2023-05-10 09:02:55 -0400

    Postgrid get html by description

--- tools/Postgrid/quickStartGuide/gettingHTMLtemplates.js
+++ tools/Postgrid/quickStartGuide/gettingHTMLtemplates.js
@@ -4,7 +4,8 @@ const POSTGRID_URL = "https://api.postgrid.com/print-mail/v1";
 const API_KEY = "test_sk_whLGEJYLGUufMEShNDYe2B";
 
 //get the html template given an ID
-async function retreiveTemplate(id) {
+async function retreiveTemplateByID(id) {
+   // request options
    const requestOptions = {
       method: "GET",
       headers: {
@@ -12,18 +13,45 @@ async function retreiveTemplate(id) {
       },
    };
 
+   //complete request to /templates
    const response = await fetch(
       POSTGRID_URL + `/templates/${id}`,
       requestOptions
    );
+   //check status of request, if successful, get as json
    if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
    }
-
    const data = await response.json();
    return data;
 }
 
-retreiveTemplate("template_g9gfaqTqkqA5eTYpLrF7LL").then((data) =>
+retreiveTemplateByID("template_g9gfaqTqkqA5eTYpLrF7LL").then((data) =>
    console.log(data)
 );
+
+//get the html template given the template description
+async function retreiveTemplateByDescription(descriptionString) {
+   // request options
+   const requestOptions = {
+      method: "GET",
+      headers: {
+         "x-api-key": API_KEY,
+      },
+   };
+   // create the query string
+   const queryString = `?skip=0&limit=10&search=${descriptionString}`;
+
+   const response = await fetch(
+      POSTGRID_URL + "/templates" + queryString,
+      requestOptions
+   );
+
+   if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+   }
+   const data = await response.json();
+   return data;
+}
+
+retreiveTemplateByDescription("html test template 2");

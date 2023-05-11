5dddd1176c0df4ed8a4bc7f9296a32fa360e34a3
Sami <52921619+sbagh@users.noreply.github.com>
2023-05-11 09:51:51 -0400

    Postgrid, get contact by first and last name

--- tools/Postgrid/quickStartGuide/2-contactsFunctions.js
+++ tools/Postgrid/quickStartGuide/2-contactsFunctions.js
@@ -1,6 +1,9 @@
 // the second step in sending a letter/postcards/.etc is creating and selecting the contacts whom you are sending to
+//contents:
 //1- Create contact
 //2- View contacts
+//   2.1- get contact by id
+//   2.2- get contact by first and last name
 
 //requirements
 import fetch from "node-fetch";
@@ -41,3 +44,68 @@ async function createContact(firstName, lastName, description, adddress) {
 //    "testing create contact API",
 //    "200 University Ave, Waterloo, ON, Canada"
 // ).then((data) => console.log(data));
+
+//2- viewing contacts
+// 2.1 - get contacts by id
+
+async function retrieveContactByID(id) {
+   const requestOptions = {
+      method: "GET",
+      headers: {
+         "x-api-key": API_KEY,
+      },
+   };
+
+   const response = await fetch(
+      POSTGRID_URL + `/contacts/${id}`,
+      requestOptions
+   );
+
+   if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+   }
+
+   return await response.json();
+}
+// retrieveContactByID("contact_jvqbcm3M21B3sQpV6yCGYK").then((data) =>
+//    console.log(data)
+// );
+
+//  2.2- get contact by first and last name
+async function getContactByName(firstName, lastName) {
+   // set request options
+   const requestOptions = {
+      method: "GET",
+      headers: {
+         "x-api-key": API_KEY,
+      },
+   };
+
+   // create search string
+   const searchString = encodeURIComponent(
+      JSON.stringify({ firstName: firstName, lastName: lastName })
+   );
+   //create the query string from the search string
+   const queryString = `?skip=0&limit=1&search=${searchString}`;
+
+   // send request
+   const response = await fetch(
+      POSTGRID_URL + `/contacts` + queryString,
+      requestOptions
+   );
+
+   // check if request was successful
+   if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+   }
+
+   const searchResults = await response.json();
+
+   // check if contact exists
+   if (searchResults.totalCount === 0) {
+      throw new Error("No such contact with given name");
+   }
+   return searchResults.data[0];
+}
+
+// getContactByName("Sami", "Bagh").then((data) => console.log(data));
